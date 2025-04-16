ad4c2391d79974169bf8a5ac250d6d099e883675
Louis <79896040+LouisDev314@users.noreply.github.com>
2025-04-15 21:40:06 -0600

    chore: add logger to exception handler

--- src/middleware/exception-handler.ts
+++ src/middleware/exception-handler.ts
@@ -1,16 +1,13 @@
 import { ErrorRequestHandler } from 'express';
 import Exception from '../errors/Exception';
 import { HttpStatusCode } from 'axios';
+import logger from '../services/logger';
 
 const exceptionHandler: ErrorRequestHandler = (err, req, res, next) => {
   if (err instanceof Exception) {
     switch (err.code) {
       case HttpStatusCode.BadRequest:
         res.send_badRequest(err.msg, err.data);
-        break;
-      case HttpStatusCode.InternalServerError:
-        // TODO: log error
-        res.send_internalServerError(err.msg, err.data);
         break;
       case HttpStatusCode.Unauthorized:
         res.send_unauthorized(err.msg, err.data);
@@ -25,11 +22,12 @@ const exceptionHandler: ErrorRequestHandler = (err, req, res, next) => {
         res.send_unprocessableEntity(err.msg, err.data);
         break;
       default:
-        res.send_internalServerError('', err);
+        logger.error(err.msg, err.data);
+        res.send_internalServerError(err.msg, err.data);
         break;
     }
   } else {
-    // TODO: log error
+    logger.error('Unhandled error', err);
     res.send_internalServerError('Unhandled error', err);
   }
 };
